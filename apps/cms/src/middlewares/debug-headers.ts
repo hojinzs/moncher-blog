4af390a9c909d0ef5a1642e86556ad266527b509
stevelee <hojinzs@gmail.com>
2025-10-01 12:48:13 +0900

    Add debug headers middleware and log environment variables in CMS server setup

---
+++ apps/cms/src/middlewares/debug-headers.ts
@@ -0,0 +1,15 @@
+export default (config, { strapi }) => {
+  return async (ctx, next) => {
+    if (ctx.path === '/admin/login' && ctx.method === 'POST') {
+      console.log('🔍 Request Headers:', {
+        'x-forwarded-proto': ctx.headers['x-forwarded-proto'],
+        'x-forwarded-ssl': ctx.headers['x-forwarded-ssl'],
+        'x-forwarded-for': ctx.headers['x-forwarded-for'],
+        'x-forwarded-host': ctx.headers['x-forwarded-host'],
+        protocol: ctx.protocol,
+        secure: ctx.secure,
+      });
+    }
+    await next();
+  };
+};
